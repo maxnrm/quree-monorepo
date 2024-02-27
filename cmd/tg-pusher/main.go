651ff1ecf44e5c5dd3feb7d0a9ab0e75753584f1
Maxim <maxim.nurmukhametov@gmail.com>
2024-02-27 23:48:49 +0500

    add tg-pusher as part of monorepo

---
+++ cmd/tg-pusher/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"quree/config"
+	"quree/internal/nats"
+	"quree/internal/telegram"
+
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/nats-io/nats.go/jetstream"
+	"golang.org/x/time/rate"
+)
+
+var ctx = context.Background()
+var tg *telegram.TelegramClient = telegram.Init(ctx)
+var nc *nats.NatsClient = nats.Init(nats.NatsSettings{
+	Ctx: ctx,
+	URL: config.NATS_URL,
+})
+
+func main() {
+
+	msgStreamConfig := jetstream.StreamConfig{
+		Name:      config.NATS_MESSAGES_STREAM,
+		Subjects:  []string{config.NATS_MESSAGES_SUBJECT},
+		Retention: jetstream.WorkQueuePolicy,
+		Storage:   jetstream.FileStorage,
+	}
+
+	msgConsumerConfig := jetstream.ConsumerConfig{
+		Durable:       config.NATS_MESSAGES_CONSUMER,
+		AckWait:       2 * time.Second,
+		MaxAckPending: 1,
+		MemoryStorage: true,
+	}
+
+	nc.CreateStream(msgStreamConfig)
+
+	cons := nc.CreateConsumer(msgStreamConfig.Name, msgConsumerConfig)
+
+	mh := createConsumeHandler(ctx)
+
+	cons.Consume(mh)
+}
+
+func createConsumeHandler(ctx context.Context) jetstream.MessageHandler {
+
+	// create tgUser type, which will store user chat_id, individual user rateLimitier from package rate,
+	// that would limit messages at 1 per sec for every user
+	// and timestamp, which store info about last message sent time
+	type tgUser struct {
+		chatID        string
+		rateLimiter   *rate.Limiter
+		latestMsgSent *timestamp.Timestamp
+	}
+
+	tgUsersMap := make(map[string]tgUser)
+
+	// create loop that would clean tgUsersMap every 10 secs from users that haven't sent messages for 10 secs
+
+	go func() {
+		for {
+			time.Sleep(10 * time.Second)
+			// loop thgour tgUsersMap and delete users that haven't sent messages for 10 secs
+			for k, v := range tgUsersMap {
+				// use time.Since
+				if time.Since(time.Unix(v.latestMsgSent.Seconds, 0)) > 10*time.Second {
+					delete(tgUsersMap, k)
+				}
+			}
+		}
+	}()
+
+	return func(msg jetstream.Msg) {
+		var msgJSON telegram.Message
+
+		// create following logic:
+		// if user is not in tgUsersMap, add him to tgUsersMap with rateLimiter and timestamp time.Now()
+		// if user exists in tgUsersMap, check if he can send message, if he can't, do msg.NackWithWait() for a sec
+		// if he can, send message and update timestamp in tgUsersMap
+
+		if _, ok := tgUsersMap[msgJSON.ChatID]; !ok {
+			limit := rate.Every(time.Second / time.Duration(config.RATE_LIMIT_PER_USER))
+			tgUsersMap[msgJSON.ChatID] = tgUser{
+				chatID:        msgJSON.ChatID,
+				rateLimiter:   rate.NewLimiter(limit, config.RATE_LIMIT_BURST),
+				latestMsgSent: &timestamp.Timestamp{Seconds: 0},
+			}
+		}
+
+		if !tgUsersMap[msgJSON.ChatID].rateLimiter.Allow() {
+			msg.NakWithDelay(1 * time.Second)
+			return
+		}
+
+		json.Unmarshal(msg.Data(), &msgJSON)
+
+		fmt.Println(msgJSON)
+		tg.SendMessage(msgJSON)
+
+		msg.DoubleAck(ctx)
+
+		*tgUsersMap[msgJSON.ChatID].latestMsgSent = timestamp.Timestamp{Seconds: time.Now().Unix()}
+
+	}
+
+}
